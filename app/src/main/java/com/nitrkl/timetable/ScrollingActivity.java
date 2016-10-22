f4412dbf645a444e063d2535db7c22e0de6401ab
Eswara <b.eswarachandra@gmail.com>
2016-10-22 17:32:48 +0530

    commit for changing calendar layout.

--- app/src/main/java/com/nitrkl/timetable/ScrollingActivity.java
+++ app/src/main/java/com/nitrkl/timetable/ScrollingActivity.java
@@ -1,12 +1,8 @@
 package com.nitrkl.timetable;
 
-import android.icu.util.Calendar;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,15 +19,6 @@ public class ScrollingActivity extends AppCompatActivity {
 //        NotificationEventReceiver.setUpPeriodAlarm(getApplicationContext(), new Period("EC", "11-10-2016", TimeSlot.DEBUG));
 
         //NotificationEventReceiver.setupAlarm(getApplicationContext());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
